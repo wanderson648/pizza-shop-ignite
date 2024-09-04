be25486d51469db925a7fa61c73e991c8cde3313
wanderson648 <testepedroca@gmail.com>
2024-09-04 00:16:53 -0300

    usando react-hook-form

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,32 +1,53 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
-    return (
-        <>
-            <div className="p-8">
-                <div className="flex w-[350px] flex-col justify-center gap-6">
-                    <div className="flex flex-col gap-2 text-center">
-                        <h1 className="text-2xl font-semibold tracking-tight">
-                            Acessar painel
-                        </h1>
-                        <p className="text-sm text-muted-foreground">
-                            Acompanhe suas vendas pelo painel do parceiro!
-                        </p>
-                    </div>
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
 
-                    <form className="space-y-4">
-                        <div className="space-y-2">
-                            <Label htmlFor="email">Seu e-mail</Label>
-                            <Input id="email" type="email" />
-                        </div>
+  async function handleSignIn(data: SignInForm) {
+    console.log(data);
 
-                        <Button className="w-full" type="submit">Acessar painel</Button>
-                    </form>
-                </div>
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+
+  return (
+    <>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register("email")} />
             </div>
-        
-        </>
-    )
+
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
